e8624d7ddc1e8c759b1c9a4dd8ca3762288029ca
Geovane Santos <geovane_n.s@outlook.com>
2022-03-04 19:39:16 -0300

    retornando links

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ import chalk from "chalk"; // precisa ser instalada pq foi desenvolvido por terc
 import fs from "fs"; // biblioteca nativa
 
 
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
-
 function extraiLinks(texto) {
     const regex = /\[([^\]]*)\]\((https?:\/\/[^s#\s].[^\s]*)\)/gm;
-    const regex1 = /https?:\/\/[^\s$.?#].[^\/\s]*\//gm;
-    const linksExtraidos = texto.match(regex);
-    const linksExtraidos1 = texto.match(regex1);
-    console.log(linksExtraidos);
-    console.log(linksExtraidos1);
+    const arrayResultados = [];
+    let temp;
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({
+            [temp[1]]: temp[2]
+        })
+    }
+    return arrayResultados;
 }
-
-extraiLinks(texto);
 
 //async/await
 function trataErro(erro) {
@@ -24,7 +23,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
     }
@@ -61,4 +60,4 @@ async function pegaArquivo(caminhoDoArquivo) {
 //     })
 // }
 
-//pegaArquivo('./arquivos/');
+pegaArquivo('./arquivos/texto1.md');

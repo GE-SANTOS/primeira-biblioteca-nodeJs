15afe8186ce35e10cf02bd1dad62ac26de2ff733
Geovane Santos <geovane_n.s@outlook.com>
2022-03-07 20:15:50 -0300

    adicionando opções

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 import chalk from "chalk";
 import { pegaArquivo } from './index.js';
+import { validaURLs } from './http-validacao.js';
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDoArquivo) {
     const resultado = await pegaArquivo(caminhoDoArquivo[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+    if (caminho[3] === 'validar') {
+        console.log(chalk.yellow('link validados'), validaURLs(resultado));
+    } else {
+        console.log(chalk.yellow('lista de links'), resultado);
+    }
 }
 
 processaTexto(caminho);

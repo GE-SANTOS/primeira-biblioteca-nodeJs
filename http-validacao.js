0777d40fa0bde752777437b27301d7601b7c4c39
Geovane Santos <geovane_n.s@outlook.com>
2022-03-09 10:33:12 -0300

    validando links

--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,12 @@
-function validaUrls(arrayLinks) {
-    return arrayLinks;
+function geraArrayDeURLs(arrayLinks) {
+    //loop para cada {cave: valor}
+    //objeto -> [valor]
+    //Object.values(objeto)
+    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+}
+
+function validaURLs(arrayLinks) {
+    return geraArrayDeURLs(arrayLinks);
 }
 
 module.exports = validaURLs;
